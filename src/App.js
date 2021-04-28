e3bddad9d6e53ed6969d5b1d204d3de2d1562b5d
Nick <marenich.mykola@gmail.com>
2021-04-28 20:43:39 +0200

    Navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-
 import './App.css';
-import navBar from './components/navBar';
+import NavBar from './components/NavBar';
+import {BrowserRouter as Router} from 'react-router-dom'
 
 function App() {
   return (
-    <div>
-      <navBar />
-    </div>
+    <Router>
+      <NavBar />
+    </Router>
   );
 }
 
--- src/components/navBar/index.js
+++ src/components/navBar/index.js
@@ -1,11 +1,37 @@
 import React from 'react'
+import {FaBars} from 'react-icons/fa'
+import {Nav, NavbarContainer, NavLogo, MobileIcon, NavMenu, NavItem, NavLinks, NavBtn, NavBtnLink } from './navBarElements'
 
-export const navBar = () => {
+export const NavBar = () => {
     return (
-        <div>
-            <h1>nav</h1>
-        </div>
+        <>
+        <Nav>
+            <NavbarContainer>
+                <NavLogo to='/' >Yolo</NavLogo>
+                <MobileIcon>
+                    <FaBars />
+                </MobileIcon>
+                <NavMenu>
+                    <NavItem>
+                        <NavLinks to='about'>About</NavLinks>
+                    </NavItem>
+                    <NavItem>
+                        <NavLinks to='discover'>Discover</NavLinks>
+                    </NavItem>
+                    <NavItem>
+                        <NavLinks to='services'>Services</NavLinks>
+                    </NavItem>
+                    <NavItem>
+                        <NavLinks to='signuup'>Sign Up</NavLinks>
+                    </NavItem>
+                </NavMenu>
+                <NavBtn>
+                    <NavBtnLink to='/signin'>Sign In</NavBtnLink>
+                </NavBtn>
+            </NavbarContainer>
+        </Nav>
+        </>
     )
 }
 
-export default navBar
+export default NavBar
