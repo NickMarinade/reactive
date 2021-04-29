1429892661103cf9a2653c31f257691013c906cc
Nick <marenich.mykola@gmail.com>
2021-04-29 21:56:51 +0200

    Sidebar

--- src/components/navBar/index.js
+++ src/components/navBar/index.js
@@ -2,13 +2,13 @@ import React from 'react'
 import {FaBars} from 'react-icons/fa'
 import {Nav, NavbarContainer, NavLogo, MobileIcon, NavMenu, NavItem, NavLinks, NavBtn, NavBtnLink } from './navBarElements'
 
-export const NavBar = () => {
+export const NavBar = ({ toggle }) => {
     return (
         <>
         <Nav>
             <NavbarContainer>
                 <NavLogo to='/' >Yolo</NavLogo>
-                <MobileIcon>
+                <MobileIcon onClick={toggle} >
                     <FaBars />
                 </MobileIcon>
                 <NavMenu>
